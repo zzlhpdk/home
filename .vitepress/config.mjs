ff833d714ef3b1440e2dce9c79929c5e61c95e65
huangyue <zzlhpdk@qq.com>
2024-12-30 10:15:10 +0800

    ✨ feat: 新增流水线

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'zzlhpdk',
   description: 'A Personal Channel',
+  base: '/protal/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: '/logo.png',
